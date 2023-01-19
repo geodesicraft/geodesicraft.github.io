c66684c13d913b3bb60161690856d7dfe3e44e3f
Jonathan Herman <jdh11235@gmail.com>
2023-01-19 16:02:50 -0600

    Add boilerplate 3d cube
    https://svelte-cubed.vercel.app/docs/getting-started

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script>
 	import Header from '$lib/Header.svelte';
+	import GeometryVisualizer from '$lib/GeometryVisualizer.svelte';
+	import Controls from '$lib/Controls.svelte';
 </script>
 
 <Header />
+<Controls />
+<GeometryVisualizer />
