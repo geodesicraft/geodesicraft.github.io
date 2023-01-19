fbce7ed3bb5187d522629242b679b8b05e9778a7
Jonathan Herman <jdh11235@gmail.com>
2023-01-19 17:23:51 -0600

    Draw a triangle
    https://threejs.org/docs/#manual/en/introduction/Drawing-lines

--- src/lib/GeometryViewer.svelte
+++ src/lib/GeometryViewer.svelte
@@ -1,12 +1,17 @@
 <script>
 	import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
+
+	const points = [];
+	points.push(new THREE.Vector3(-1, 0, 0));
+	points.push(new THREE.Vector3(0, 1, 0));
+	points.push(new THREE.Vector3(1, 0, 0));
 </script>
 
 <div>
 	<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
-		<SC.Mesh geometry={new THREE.BoxGeometry()} />
-		<SC.PerspectiveCamera position={[1, 1, 3]} />
+		<SC.Mesh geometry={new THREE.BufferGeometry().setFromPoints(points)} />
+		<SC.PerspectiveCamera position={[-2, 2, -2]} />
 		<SC.OrbitControls enableZoom={false} />
 	</SC.Canvas>
 </div>
