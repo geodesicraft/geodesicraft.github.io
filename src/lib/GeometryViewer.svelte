82a810da307a504daa2b307f0cfcde3d17d0b247
Jonathan Herman <jdh11235@gmail.com>
2023-01-20 17:18:25 -0600

    Switch Svelte Cubed to Threlte
    https://threlte.xyz/

--- src/lib/GeometryViewer.svelte
+++ src/lib/GeometryViewer.svelte
@@ -1,24 +1,58 @@
 <script>
-	import * as THREE from 'three';
-	import * as SC from 'svelte-cubed';
+	import { Canvas, InteractiveObject, OrbitControls, Three } from '@threlte/core';
+	import { spring } from 'svelte/motion';
+	import {
+		AmbientLight,
+		BoxGeometry,
+		CircleGeometry,
+		DirectionalLight,
+		Group,
+		Mesh,
+		MeshStandardMaterial,
+		PerspectiveCamera
+	} from 'three';
+	import { degToRad } from 'three/src/math/MathUtils';
 
-	const points = [];
-	points.push(new THREE.Vector3(-1, 0, 0));
-	points.push(new THREE.Vector3(0, 1, 0));
-	points.push(new THREE.Vector3(1, 0, 0));
+	const scale = spring(1);
 </script>
 
 <div>
-	<SC.Canvas antialias background={new THREE.Color('papayawhip')}>
-		<SC.Mesh geometry={new THREE.BufferGeometry().setFromPoints(points)} />
-		<SC.PerspectiveCamera position={[-2, 2, -2]} />
-		<SC.OrbitControls enableZoom={false} />
-	</SC.Canvas>
+	<Canvas>
+		<Three type={PerspectiveCamera} makeDefault position={[10, 10, 10]} fov={24}>
+			<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 0.5 }} />
+		</Three>
+
+		<Three type={DirectionalLight} castShadow position={[3, 10, 10]} />
+		<Three type={DirectionalLight} position={[-3, 10, -10]} intensity={0.2} />
+		<Three type={AmbientLight} intensity={0.2} />
+
+		<!-- Cube -->
+		<Three type={Group} scale={$scale}>
+			<Three type={Mesh} position.y={0.5} castShadow let:ref>
+				<!-- Add interaction -->
+				<InteractiveObject
+					object={ref}
+					interactive
+					on:pointerenter={() => ($scale = 2)}
+					on:pointerleave={() => ($scale = 1)}
+				/>
+
+				<Three type={BoxGeometry} />
+				<Three type={MeshStandardMaterial} color="#333333" />
+			</Three>
+		</Three>
+
+		<!-- Floor -->
+		<Three type={Mesh} receiveShadow rotation.x={degToRad(-90)}>
+			<Three type={CircleGeometry} args={[3, 72]} />
+			<Three type={MeshStandardMaterial} color="white" />
+		</Three>
+	</Canvas>
 </div>
 
 <style>
 	div {
-		background: grey;
-		position: relative;
+		height: 100%;
+		width: 100%;
 	}
 </style>
