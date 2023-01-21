59051184c2c6ac95b6a094f3fcfac03fe22c38d0
Jonathan Herman <jdh11235@gmail.com>
2023-01-21 17:19:51 -0600

    Use threlte Line2 example
    https://threlte.xyz/core/line-2

---
+++ src/lib/Scene.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
+	import { DirectionalLight, Line2, OrbitControls, PerspectiveCamera } from '@threlte/core';
+
+	const lines = [];
+	for (let index = 0; index < 10; index++) {
+		const points = [];
+		for (let index = 0; index < 10; index++) {
+			const point = [25 - Math.random() * 50, 25 - Math.random() * 50, 25 - Math.random() * 50];
+			points.push(point);
+		}
+		lines.push(points);
+	}
+
+	const getRandomColor = () => {
+		return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	};
+</script>
+
+<PerspectiveCamera position={{ y: 100, x: 100, z: 100 }}>
+	<OrbitControls autoRotate enableZoom={true} />
+</PerspectiveCamera>
+
+<DirectionalLight position={{ y: 10 }} />
+
+{#each lines as line}
+	<Line2
+		material={new LineMaterial({
+			worldUnits: true,
+			linewidth: 1,
+			color: getRandomColor()
+		})}
+		points={line}
+	/>
+{/each}
