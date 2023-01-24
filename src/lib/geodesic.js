c8e31d05059a3f1a57a1f7adbc61f9af723ed4aa
Jonathan Herman <jdh11235@gmail.com>
2023-01-24 06:47:32 -0600

    Create vertices using spherical coordinates
    https://en.wikipedia.org/wiki/Spherical_coordinate_system#Cartesian_coordinates

--- src/lib/geodesic.js
+++ src/lib/geodesic.js
@@ -1,56 +1,26 @@
 import { edges, vertices, faces } from '$lib/stores.js';
 
 export function createGeodesic() {
-	setEdges();
-	setVertices();
-	setFaces();
+	const sphericalVertices = [
+		[1, 0, 0],
+		[1, 1, 0],
+		[1, 1, 1]
+	];
+	const cartesianVertices = sphericalVertices.map((sphericalVertex) =>
+		sphericalToCartesian(...sphericalVertex)
+	);
+	vertices.set(cartesianVertices);
+
+	edges.set([]);
+
+	faces.set([]);
 }
 
-function setEdges() {
-	edges.set([
-		[
-			[0, 0, 0],
-			[1, 1, 1]
-		],
-		[
-			[0, 0, 0],
-			[1, -1, 1]
-		],
-		[
-			[0, 0, 0],
-			[-1, -1, 1]
-		],
-		[
-			[1, 1, 1],
-			[1, -1, 1]
-		],
-		[
-			[1, -1, 1],
-			[-1, -1, 1]
-		]
-	]);
+function sphericalToCartesian(rho, theta, phi) {
+	// radius, inclination, aziumuth
+	const x = rho * Math.sin(theta) * Math.cos(phi);
+	const y = rho * Math.sin(theta) * Math.sin(phi);
+	const z = rho * Math.cos(theta);
+	return [x, y, z];
 }
-
-function setVertices() {
-	vertices.set([
-		[0, 0, 0],
-		[1, 1, 1],
-		[1, -1, 1],
-		[-1, -1, 1]
-	]);
-}
-
-function setFaces() {
-	faces.set([
-		[
-			[0, 0, 0],
-			[1, 1, 1],
-			[1, -1, 1]
-		],
-		[
-			[0, 0, 0],
-			[1, -1, 1],
-			[-1, -1, 1]
-		]
-	]);
-}
+// https://en.wikipedia.org/wiki/Spherical_coordinate_system#Cartesian_coordinates
