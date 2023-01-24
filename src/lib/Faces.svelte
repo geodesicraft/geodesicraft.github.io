34a1c7dc4ab9afdef053265946ff247d9b0b17be
Jonathan Herman <jdh11235@gmail.com>
2023-01-24 01:31:08 -0600

    Render faces as a closed shape
    https://threlte.xyz/core/mesh
    
    https://stackoverflow.com/a/37863323
    
    https://threejs.org/docs/#examples/en/geometries/ConvexGeometry
    
    https://threejs.org/examples/#webgl_geometry_convex
    
    https://github.com/mrdoob/three.js/blob/master/examples/webgl_geometry_convex.html

--- src/lib/Faces.svelte
+++ src/lib/Faces.svelte
@@ -1,5 +1,26 @@
 <script>
+	import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry';
+	import { Mesh } from '@threlte/core';
+	import { MeshLambertMaterial, Vector3 } from 'three';
 	import { faces } from '$lib/stores.js';
+	import { getRandomColor } from '$lib/getRandomColor.js';
+
+	const points = [
+		new Vector3(0, 0, 0),
+		new Vector3(1, 1, 1),
+		new Vector3(1, -1, 1),
+		new Vector3(-1, -1, 1)
+	];
+
+	const geometry = new ConvexGeometry(points);
+
+	const material = new MeshLambertMaterial({
+		color: 0x333333,
+		opacity: 0.5,
+		transparent: true
+	});
 
 	$: console.log('faces:', $faces);
 </script>
+
+<Mesh position={new Vector3(0, 0, 0)} {geometry} {material} />
