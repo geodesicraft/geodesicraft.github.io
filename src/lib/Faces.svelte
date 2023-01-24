4d5d9250689872d7a5b319289e6db6795617d86d
Jonathan Herman <jdh11235@gmail.com>
2023-01-24 04:23:56 -0600

    Switch face material to doubleside

--- src/lib/Faces.svelte
+++ src/lib/Faces.svelte
@@ -1,26 +1,21 @@
 <script>
 	import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry';
 	import { Mesh } from '@threlte/core';
-	import { BackSide, FrontSide, MeshLambertMaterial, Vector3 } from 'three';
+	import { DoubleSide, MeshLambertMaterial, Vector3 } from 'three';
 	import { faces, vertices } from '$lib/stores.js';
 
 	const points = $vertices.map((vertex) => new Vector3(...vertex));
 
 	const geometry = new ConvexGeometry(points);
 
-	const frontMaterial = new MeshLambertMaterial({
+	const material = new MeshLambertMaterial({
 		color: 0x333333,
 		opacity: 0.5,
-		transparent: true
+		transparent: true,
+		side: DoubleSide
 	});
-
-	const backMaterial = frontMaterial.clone();
-
-	frontMaterial.side = FrontSide;
-	backMaterial.side = BackSide;
 
 	$: console.log('faces:', $faces);
 </script>
 
-<Mesh {geometry} material={frontMaterial} renderOrder="1" />
-<Mesh {geometry} material={backMaterial} renderOrder="0" />
+<Mesh {geometry} {material} />
