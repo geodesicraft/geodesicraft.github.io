43db8b0b546634f0014df7ca7f6602d2fe34a20c
Jonathan Herman <jdh11235@gmail.com>
2023-01-23 23:41:39 -0600

    Move general scene stuff up one level

--- src/lib/GeometryViewer.svelte
+++ src/lib/GeometryViewer.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { Canvas } from '@threlte/core';
+	import { Canvas, DirectionalLight, OrbitControls, PerspectiveCamera } from '@threlte/core';
 	import Lines from '$lib/Lines.svelte';
 	import Vertices from '$lib/Vertices.svelte';
 	import Faces from '$lib/Faces.svelte';
@@ -7,6 +7,12 @@
 
 <div>
 	<Canvas>
+		<PerspectiveCamera position={{ y: 3, x: 3, z: 3 }}>
+			<OrbitControls autoRotate enableZoom={true} />
+		</PerspectiveCamera>
+
+		<DirectionalLight position={{ y: 10 }} />
+
 		<Lines />
 		<Vertices />
 		<Faces />
--- src/lib/Lines.svelte
+++ src/lib/Lines.svelte
@@ -1,17 +1,11 @@
 <script>
 	import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
-	import { DirectionalLight, Line2, OrbitControls, PerspectiveCamera } from '@threlte/core';
+	import { Line2 } from '@threlte/core';
 	import { lines } from '$lib/stores.js';
 	import { getRandomColor } from '$lib/getRandomColor.js';
 
 	$: console.log('lines:', $lines);
 </script>
-
-<PerspectiveCamera position={{ y: 3, x: 3, z: 3 }}>
-	<OrbitControls autoRotate enableZoom={true} />
-</PerspectiveCamera>
-
-<DirectionalLight position={{ y: 10 }} />
 
 {#each $lines as line}
 	<Line2
