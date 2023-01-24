8dec61e0395c7c94f6a91b15afff743246a7926e
Jonathan Herman <jdh11235@gmail.com>
2023-01-23 21:41:13 -0600

    Manually specify lines

--- src/lib/Scene.svelte
+++ src/lib/Scene.svelte
@@ -2,22 +2,27 @@
 	import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
 	import { DirectionalLight, Line2, OrbitControls, PerspectiveCamera } from '@threlte/core';
 
-	const lines = [];
-	for (let index = 0; index < 10; index++) {
-		const points = [];
-		for (let index = 0; index < 10; index++) {
-			const point = [25 - Math.random() * 50, 25 - Math.random() * 50, 25 - Math.random() * 50];
-			points.push(point);
-		}
-		lines.push(points);
-	}
+	const lines = [
+		[
+			[0, 0, 0],
+			[1, 1, 1]
+		],
+		[
+			[0, 0, 0],
+			[1, -1, 1]
+		],
+		[
+			[0, 0, 0],
+			[-1, -1, 1]
+		]
+	];
 
 	const getRandomColor = () => {
 		return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 	};
 </script>
 
-<PerspectiveCamera position={{ y: 100, x: 100, z: 100 }}>
+<PerspectiveCamera position={{ y: 3, x: 3, z: 3 }}>
 	<OrbitControls autoRotate enableZoom={true} />
 </PerspectiveCamera>
 
@@ -27,7 +32,7 @@
 	<Line2
 		material={new LineMaterial({
 			worldUnits: true,
-			linewidth: 1,
+			linewidth: 0.05,
 			color: getRandomColor()
 		})}
 		points={line}
