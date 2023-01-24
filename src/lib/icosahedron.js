4571f04863fad585af7717080617f32fdb571270
Jonathan Herman <jdh11235@gmail.com>
2023-01-24 11:59:15 -0600

    Use radians for spherical coordinates

--- src/lib/icosahedron.js
+++ src/lib/icosahedron.js
@@ -1,6 +1,11 @@
 export const createIcosahedron = function () {
 	return {
-		sphericalVertices: [],
+		sphericalVertices: [
+			[1, Math.PI * 0, 0],
+			[1, Math.PI * 0.5, 0],
+			[1, Math.PI * 1, 0],
+			[1, Math.PI * 1.5, 0]
+		],
 		sphericalEdges: [],
 		sphericalFaces: []
 	};
