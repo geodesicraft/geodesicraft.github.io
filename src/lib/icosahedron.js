4de73c91dacd9ed4f0564cef2a9dfcb1f37af3be
Jonathan Herman <jdh11235@gmail.com>
2023-01-24 12:03:08 -0600

    Add top and bottom vertices

--- src/lib/icosahedron.js
+++ src/lib/icosahedron.js
@@ -1,10 +1,13 @@
 export const createIcosahedron = function () {
 	return {
+		// radians
 		sphericalVertices: [
+			[1, Math.PI * 0.5, Math.PI * 0.5],
 			[1, Math.PI * 0, 0],
 			[1, Math.PI * 0.5, 0],
 			[1, Math.PI * 1, 0],
-			[1, Math.PI * 1.5, 0]
+			[1, Math.PI * 1.5, 0],
+			[1, Math.PI * 0.5, Math.PI * 1.5]
 		],
 		sphericalEdges: [],
 		sphericalFaces: []
