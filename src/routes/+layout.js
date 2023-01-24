4ab575fc7d65bd5a670993c32f809318863814ed
Jonathan Herman <jdh11235@gmail.com>
2023-01-23 22:17:07 -0600

    Clear console on Vite HMR Reload
    https://github.com/vitejs/vite/discussions/3143#discussioncomment-1717076

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,12 @@
 export const prerender = true;
 export const trailingSlash = 'always';
+
+// Clear console on Vite HMR reload
+// https://github.com/vitejs/vite/discussions/3143#discussioncomment-1717076
+if (import.meta.hot) {
+	import.meta.hot.on(
+		'vite:beforeUpdate',
+		/* eslint-disable-next-line no-console */
+		() => console.clear()
+	);
+}
