c94a6f21d95cfb0bbaf7c7e5eb5354f798491523
Jonathan Herman <jdh11235@gmail.com>
2023-01-24 04:54:07 -0600

    Close #4

---
+++ src/lib/Face.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Mesh } from '@threlte/core';
+	import { BufferAttribute, BufferGeometry, DoubleSide, MeshLambertMaterial } from 'three';
+
+	export let face;
+
+	const geometry = new BufferGeometry();
+	const verticesMatrix = new Float32Array([...face[0], ...face[1], ...face[2]]);
+	geometry.setAttribute('position', new BufferAttribute(verticesMatrix, 3));
+
+	const material = new MeshLambertMaterial({
+		color: 0x333333,
+		opacity: 0.5,
+		transparent: true,
+		side: DoubleSide
+	});
+</script>
+
+<Mesh {geometry} {material} />
--- src/lib/Faces.svelte
+++ src/lib/Faces.svelte
@@ -1,22 +1,10 @@
 <script>
-	import { Mesh } from '@threlte/core';
-	import { BufferAttribute, BufferGeometry, DoubleSide, MeshLambertMaterial, Vector3 } from 'three';
+	import Face from '$lib/Face.svelte';
 	import { faces } from '$lib/stores.js';
-
-	const verticesMatrix = new Float32Array([...$faces[0][0], ...$faces[0][1], ...$faces[0][2]]);
-
-	const geometry = new BufferGeometry();
-
-	geometry.setAttribute('position', new BufferAttribute(verticesMatrix, 3));
-
-	const material = new MeshLambertMaterial({
-		color: 0x333333,
-		opacity: 0.5,
-		transparent: true,
-		side: DoubleSide
-	});
 
 	$: console.log('faces:', $faces);
 </script>
 
-<Mesh {geometry} {material} />
+{#each $faces as face}
+	<Face {face} />
+{/each}
