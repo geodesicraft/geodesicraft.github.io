059f70aff7ac415ce1ba2c9de2e847b2d092583e
Jonathan Herman <jdh11235@gmail.com>
2023-01-24 04:34:47 -0600

    Get an individual face to render, see #4

--- src/lib/Faces.svelte
+++ src/lib/Faces.svelte
@@ -1,12 +1,14 @@
 <script>
 	import { ConvexGeometry } from 'three/examples/jsm/geometries/ConvexGeometry';
 	import { Mesh } from '@threlte/core';
-	import { DoubleSide, MeshLambertMaterial, Vector3 } from 'three';
+	import { BufferAttribute, BufferGeometry, DoubleSide, MeshLambertMaterial, Vector3 } from 'three';
 	import { faces, vertices } from '$lib/stores.js';
 
-	const points = $vertices.map((vertex) => new Vector3(...vertex));
+	const verticesMatrix = new Float32Array([0, 0, 0, 1, 1, 1, 1, -1, 1]);
 
-	const geometry = new ConvexGeometry(points);
+	const geometry = new BufferGeometry();
+
+	geometry.setAttribute('position', new BufferAttribute(verticesMatrix, 3));
 
 	const material = new MeshLambertMaterial({
 		color: 0x333333,
