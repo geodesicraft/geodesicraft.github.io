b093d7425a2bcbd57b6ef9d0144f2feb9308bc56
Jonathan Herman <jdh11235@gmail.com>
2023-01-30 23:37:14 -0600

    Closes #27, creates #29

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store';
 import { browser } from '$app/environment';
 
+//
+
 const viewerSettingsParameters = {
 	autoRotate: {
 		default: true
@@ -12,7 +14,6 @@ if (browser) {
 	const localViewerSettings = JSON.parse(window.localStorage.getItem('viewerSettings'));
 	if (localViewerSettings !== null) {
 		for (const [key, value] of Object.entries(viewerSettingsParameters)) {
-			console.log(key, value);
 			if (localViewerSettings[key] === undefined) {
 				mergedViewerSettings[key] = value.default;
 			} else {
@@ -30,10 +31,40 @@ viewerSettings.subscribe((value) => {
 	}
 });
 
-export const domeSettings = writable({
-	vertexSize: 0.15,
-	edgeThickness: 0.05
+//
+
+const domeSettingsParameters = {
+	vertexSize: {
+		default: 0.15
+	},
+	edgeThickness: {
+		default: 0.05
+	}
+};
+
+let mergedDomeSettings = {};
+if (browser) {
+	const localDomeSettings = JSON.parse(window.localStorage.getItem('domeSettings'));
+	if (localDomeSettings !== null) {
+		for (const [key, value] of Object.entries(domeSettingsParameters)) {
+			if (localDomeSettings[key] === undefined) {
+				mergedDomeSettings[key] = value.default;
+			} else {
+				mergedDomeSettings[key] = localDomeSettings[key];
+			}
+		}
+	}
+}
+
+export const domeSettings = writable(mergedDomeSettings);
+
+domeSettings.subscribe((value) => {
+	if (browser) {
+		window.localStorage.setItem('domeSettings', JSON.stringify(value));
+	}
 });
+
+//
 
 export const domeData = writable({
 	edges: [],
