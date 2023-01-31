6e2058893fc0b5be050d2196bc6eb826d20847fb
jonthesquirrel <jonthesquirrel@users.noreply.github.com>
2023-01-31 02:04:08 +0000

    deploy: 2dc14192e5516114f98c7bcac1103137ba203602

---
+++ _app/immutable/chunks/2-01b571b0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-4cc8e230.js";export{t as component};
--- _app/immutable/chunks/2-440d3d1c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-be0cd08e.js";export{t as component};
