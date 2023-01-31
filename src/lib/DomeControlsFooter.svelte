98718f71e08c0ccc7f6a3fe0a107d3c40b758f64
Jonathan Herman <jdh11235@gmail.com>
2023-01-31 06:47:06 -0600

    Close #36

--- src/lib/DomeControlsFooter.svelte
+++ src/lib/DomeControlsFooter.svelte
@@ -1,9 +1,28 @@
-<div>Dome Controls Footer</div>
+<script>
+	import { domeSettings, domeSettingsParameters } from '$lib/stores.js';
+	import Slider from '$lib/Slider.svelte';
+</script>
+
+<div>
+	{#each Object.entries(domeSettingsParameters) as [key, value] (key)}
+		<Slider
+			name={value.name}
+			bind:value={$domeSettings[key]}
+			min={value.min}
+			max={value.max}
+			step={value.step}
+		/>
+	{/each}
+</div>
 
 <style>
 	div {
-		background: #d3d3d3;
-		height: 5rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 		padding: 1rem;
+		height: 5rem;
+		background: #d3d3d3;
+		overflow: scroll;
 	}
 </style>
