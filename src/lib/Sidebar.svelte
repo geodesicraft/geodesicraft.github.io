9dcd9d2782b7ca6a17b85d5118b23feb2caa85a0
Jonathan Herman <jdh11235@gmail.com>
2023-02-13 20:49:18 -0600

    Close #62

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -3,7 +3,7 @@
 	import DomeControlsSidebar from '$lib/DomeControlsSidebar.svelte';
 </script>
 
-<div class="h-full grid grid-rows-[auto_1fr] overflow-hidden">
+<div class="h-full grid grid-rows-[auto_1fr] overflow-hidden w-72">
 	<ViewerControls />
 	<DomeControlsSidebar />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,66 +16,29 @@
 	/>
 </svelte:head>
 
-<div class="main bg-zinc-100 dark:bg-zinc-600 grid h-screen">
-	<div class="sidebar">
+<svelte:body class="m-0 p-0 overflow-hidden font-sans " />
+
+<div
+	class="main bg-zinc-100 dark:bg-zinc-600 grid h-screen grid-cols-1 grid-rows-[auto_1fr_auto] md:grid-cols-[auto_1fr]"
+>
+	<div class="sidebar h-screen max-md:hidden">
 		<Sidebar />
 	</div>
-	<div class="header">
+	<div class="header md:hidden">
 		<Header />
 	</div>
 	<ViewerArea />
-	<div class="footer">
+	<div class="footer md:hidden">
 		<Footer />
 	</div>
 </div>
 
 <style lang="scss">
-	$sidebar-width: 17rem;
-
-	:global(*) {
-		font-family: sans-serif;
-	}
-
-	:global(body) {
-		margin: 0;
-		padding: 0;
-		overflow: hidden;
-	}
-
 	:global(.clickthroughable > *) {
 		pointer-events: auto;
 	}
 
 	:global(.clickthroughable) {
 		pointer-events: none;
-	}
-
-	// wide view
-
-	.main {
-		grid-template-columns: $sidebar-width 1fr;
-	}
-
-	.header,
-	.footer {
-		display: none;
-	}
-
-	// narrow view
-
-	@media (max-width: ($sidebar-width * 2.5)) {
-		.main {
-			grid-template-columns: 1fr;
-			grid-template-rows: auto 1fr auto;
-		}
-
-		.sidebar {
-			display: none;
-		}
-
-		.header,
-		.footer {
-			display: initial;
-		}
 	}
 </style>
