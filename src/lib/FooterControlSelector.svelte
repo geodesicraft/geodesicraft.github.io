1cf7793c716e18d0773b03f0103d3fdfcd872b80
Jonathan Herman <jdh11235@gmail.com>
2023-02-13 22:33:42 -0600

    Fix <select> inconsistencies
    with desktop hover and mobile padding

--- src/lib/FooterControlSelector.svelte
+++ src/lib/FooterControlSelector.svelte
@@ -1,12 +1,12 @@
 <script>
-	import { domeSettings, domeSettingsParameters } from '$lib/stores.js';
+	import { domeSettingsParameters } from '$lib/stores.js';
 
 	export let selected;
 </script>
 
 <select
 	bind:value={selected}
-	class="bg-zinc-200 dark:bg-zinc-600 text-zinc-900 dark:text-zinc-100 cursor-pointer p-2 rounded-lg tracking-wide w-full"
+	class="bg-zinc-400 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 cursor-pointer p-2 rounded-lg tracking-wide w-full"
 >
 	{#each Object.entries(domeSettingsParameters) as [key, value] (key)}
 		<option value={key}>{value.title}</option>
