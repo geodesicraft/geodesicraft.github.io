45fe122343754ee610088f2f8298ee2896b46c61
Jonathan Herman <jdh11235@gmail.com>
2023-02-14 05:49:14 -0600

    Close #81

--- src/lib/CopyButton.svelte
+++ src/lib/CopyButton.svelte
@@ -4,7 +4,7 @@
 
 <div
 	title="Copy Command"
-	class="cursor-pointer p-3 bg-zinc-100 dark:bg-zinc-600 hover:!bg-blue-500 text-zinc-900 dark:text-zinc-100  rounded-l-lg"
+	class="cursor-pointer p-3 bg-zinc-100 dark:bg-zinc-600 hover:!bg-blue-500 text-zinc-900 dark:text-zinc-100 hover:!text-zinc-100 rounded-l-lg"
 >
 	<MdiContentCopy />
 </div>
--- src/lib/IconToggle.svelte
+++ src/lib/IconToggle.svelte
@@ -6,6 +6,7 @@
 <label
 	{title}
 	class:!bg-blue-500={checked}
+	class:!text-zinc-100={checked}
 	class="bg-zinc-400 dark:bg-zinc-800 text-zinc-900 dark:text-zinc-100 cursor-pointer select-none rounded-lg p-3.5"
 >
 	<input type="checkbox" bind:checked class="cursor-pointer hidden" />
