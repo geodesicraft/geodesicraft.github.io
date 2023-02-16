ca2847723078dc0b39d896d15251cbe9640f976a
Jonathan Herman <jdh11235@gmail.com>
2023-02-16 04:40:08 -0600

    Move cartesian and spherical conversion outside

--- src/lib/Coordinates.js
+++ src/lib/Coordinates.js
@@ -1,23 +1,7 @@
+import cartesianToSpherical from '$lib/cartesianToSpherical.js';
+import sphericalToCartesian from '$lib/sphericalToCartesian.js';
+
 export default class Coordinates {
-	static #sphericalToCartesian([rho, theta, phi]) {
-		// radius, inclination, aziumuth
-		const x = rho * Math.sin(theta) * Math.sin(phi);
-		const y = rho * Math.cos(theta);
-		const z = rho * Math.sin(theta) * Math.cos(phi);
-		return [x, y, z];
-	}
-
-	// https://en.wikipedia.org/wiki/Spherical_coordinate_system#Cartesian_coordinates
-	// These formulas are modified so x, y, z is consistent with Minecraft and Three.js
-
-	static #cartesianToSpherical([x, y, z]) {
-		// TODO
-		const rho = Math.sqrt((x ^ 2) + (y ^ 2) + (z ^ 2));
-		const theta = 0;
-		const phi = 0;
-		return [rho, theta, phi];
-	}
-
 	#rho;
 	#theta;
 	#phi;
@@ -26,18 +10,20 @@ export default class Coordinates {
 		return [this.#rho, this.#theta, this.#phi];
 	}
 
-	setSpherical(sphericalCoordinates) {
-		[this.#rho, this.#theta, this.#phi] = sphericalCoordinates;
+	setSpherical(rho, theta, phi) {
+		this.#rho = rho;
+		this.#theta = theta;
+		this.#phi = phi;
 
 		return this;
 	}
 
 	getCartesian() {
-		return Coordinates.#sphericalToCartesian([this.#rho, this.#theta, this.#phi]);
+		return sphericalToCartesian(...this.getSpherical());
 	}
 
-	setCartesian([x, y, z]) {
-		[this.#rho, this.#theta, this.#phi] = Coordinates.#cartesianToSpherical([x, y, z]);
+	setCartesian(x, y, z) {
+		this.setSpherical(...cartesianToSpherical(x, y, z));
 
 		return this;
 	}
