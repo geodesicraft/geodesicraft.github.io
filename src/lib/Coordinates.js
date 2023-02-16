31a2fc43d1071ea24b6423f185559c37bba39016
Jonathan Herman <jdh11235@gmail.com>
2023-02-15 22:59:30 -0600

    Add WIP progress on #88

--- src/lib/Coordinates.js
+++ src/lib/Coordinates.js
@@ -6,12 +6,13 @@ export default class Coordinates {
 		const z = rho * Math.sin(theta) * Math.cos(phi);
 		return [x, y, z];
 	}
+
 	// https://en.wikipedia.org/wiki/Spherical_coordinate_system#Cartesian_coordinates
-	// This formula is modified so x, y, z is consistent with Minecraft and Three.js
+	// These formulas are modified so x, y, z is consistent with Minecraft and Three.js
 
 	static #cartesianToSpherical([x, y, z]) {
 		// TODO
-		const rho = 0;
+		const rho = Math.sqrt((x ^ 2) + (y ^ 2) + (z ^ 2));
 		const theta = 0;
 		const phi = 0;
 		return [rho, theta, phi];
