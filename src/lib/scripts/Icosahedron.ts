27f9f65d41e0756a5e239a71e278d2985e86ebba
Jonathan Herman <jdh11235@gmail.com>
2023-03-09 07:52:14 -0600

    Change edges to new method

--- src/lib/scripts/Icosahedron.ts
+++ src/lib/scripts/Icosahedron.ts
@@ -8,61 +8,56 @@ export default class Icosahedron {
 	edges: Array<Edge>;
 	faces: Array<Face>;
 
-	#phi = (1 + Math.sqrt(5)) / 2; // Golden ratio
-
 	constructor() {
+		const phi = (1 + Math.sqrt(5)) / 2; // Golden ratio
+
 		this.vertices = [
-			[-1, this.#phi, 0],
-			[1, this.#phi, 0],
-			[-1, -this.#phi, 0],
-			[1, -this.#phi, 0],
-			[0, -1, this.#phi],
-			[0, 1, this.#phi],
-			[0, -1, -this.#phi],
-			[0, 1, -this.#phi],
-			[this.#phi, 0, -1],
-			[this.#phi, 0, 1],
-			[-this.#phi, 0, -1],
-			[-this.#phi, 0, 1]
-		].map((arr) => new Vertex().setCartesian(<Vector3Tuple>arr));
+			[-1, phi, 0],
+			[1, phi, 0],
+			[-1, -phi, 0],
+			[1, -phi, 0],
+			[0, -1, phi],
+			[0, 1, phi],
+			[0, -1, -phi],
+			[0, 1, -phi],
+			[phi, 0, -1],
+			[phi, 0, 1],
+			[-phi, 0, -1],
+			[-phi, 0, 1]
+		].map((vertex) => new Vertex().setCartesian(<Vector3Tuple>vertex));
 
 		this.edges = [
-			// zig zag ring
-			new Edge().setVertices([this.vertices[0], this.vertices[1]]),
-			new Edge().setVertices([this.vertices[1], this.vertices[2]]),
-			new Edge().setVertices([this.vertices[2], this.vertices[3]]),
-			new Edge().setVertices([this.vertices[3], this.vertices[4]]),
-			new Edge().setVertices([this.vertices[4], this.vertices[5]]),
-			new Edge().setVertices([this.vertices[5], this.vertices[6]]),
-			new Edge().setVertices([this.vertices[6], this.vertices[7]]),
-			new Edge().setVertices([this.vertices[7], this.vertices[8]]),
-			new Edge().setVertices([this.vertices[8], this.vertices[9]]),
-			new Edge().setVertices([this.vertices[9], this.vertices[0]]),
-			// upper ring
-			new Edge().setVertices([this.vertices[0], this.vertices[2]]),
-			new Edge().setVertices([this.vertices[2], this.vertices[4]]),
-			new Edge().setVertices([this.vertices[4], this.vertices[6]]),
-			new Edge().setVertices([this.vertices[6], this.vertices[8]]),
-			new Edge().setVertices([this.vertices[8], this.vertices[0]]),
-			// lower ring
-			new Edge().setVertices([this.vertices[1], this.vertices[3]]),
-			new Edge().setVertices([this.vertices[3], this.vertices[5]]),
-			new Edge().setVertices([this.vertices[5], this.vertices[7]]),
-			new Edge().setVertices([this.vertices[7], this.vertices[9]]),
-			new Edge().setVertices([this.vertices[9], this.vertices[1]]),
-			// top
-			new Edge().setVertices([this.vertices[10], this.vertices[0]]),
-			new Edge().setVertices([this.vertices[10], this.vertices[2]]),
-			new Edge().setVertices([this.vertices[10], this.vertices[4]]),
-			new Edge().setVertices([this.vertices[10], this.vertices[6]]),
-			new Edge().setVertices([this.vertices[10], this.vertices[8]]),
-			// bottom
-			new Edge().setVertices([this.vertices[11], this.vertices[1]]),
-			new Edge().setVertices([this.vertices[11], this.vertices[3]]),
-			new Edge().setVertices([this.vertices[11], this.vertices[5]]),
-			new Edge().setVertices([this.vertices[11], this.vertices[7]]),
-			new Edge().setVertices([this.vertices[11], this.vertices[9]])
-		];
+			[0, 1],
+			[0, 4],
+			[0, 5],
+			[0, 8],
+			[0, 10],
+			[1, 6],
+			[1, 7],
+			[1, 8],
+			[1, 10],
+			[1, 11],
+			[2, 3],
+			[2, 4],
+			[2, 5],
+			[2, 9],
+			[2, 11],
+			[3, 6],
+			[3, 7],
+			[3, 9],
+			[3, 10],
+			[3, 11],
+			[4, 8],
+			[4, 9],
+			[4, 10],
+			[5, 9],
+			[5, 10],
+			[5, 11],
+			[6, 7],
+			[6, 9],
+			[7, 8],
+			[7, 11]
+		].map((edge) => new Edge().setVertices([this.vertices[edge[0]], this.vertices[edge[1]]]));
 
 		this.faces = [
 			// ring
