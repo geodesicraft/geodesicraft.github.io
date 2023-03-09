280b4fc9a6ae02b208cb184795faae3475e80578
Jonathan Herman <jdh11235@gmail.com>
2023-03-09 07:53:59 -0600

    Change faces to new method

--- src/lib/scripts/Icosahedron.ts
+++ src/lib/scripts/Icosahedron.ts
@@ -60,29 +60,32 @@ export default class Icosahedron {
 		].map((edge) => new Edge().setVertices([this.vertices[edge[0]], this.vertices[edge[1]]]));
 
 		this.faces = [
-			// ring
-			new Face().setVertices([this.vertices[0], this.vertices[1], this.vertices[2]]),
-			new Face().setVertices([this.vertices[1], this.vertices[2], this.vertices[3]]),
-			new Face().setVertices([this.vertices[2], this.vertices[3], this.vertices[4]]),
-			new Face().setVertices([this.vertices[3], this.vertices[4], this.vertices[5]]),
-			new Face().setVertices([this.vertices[4], this.vertices[5], this.vertices[6]]),
-			new Face().setVertices([this.vertices[5], this.vertices[6], this.vertices[7]]),
-			new Face().setVertices([this.vertices[6], this.vertices[7], this.vertices[8]]),
-			new Face().setVertices([this.vertices[7], this.vertices[8], this.vertices[9]]),
-			new Face().setVertices([this.vertices[8], this.vertices[9], this.vertices[0]]),
-			new Face().setVertices([this.vertices[9], this.vertices[0], this.vertices[1]]),
-			// top
-			new Face().setVertices([this.vertices[10], this.vertices[0], this.vertices[2]]),
-			new Face().setVertices([this.vertices[10], this.vertices[2], this.vertices[4]]),
-			new Face().setVertices([this.vertices[10], this.vertices[4], this.vertices[6]]),
-			new Face().setVertices([this.vertices[10], this.vertices[6], this.vertices[8]]),
-			new Face().setVertices([this.vertices[10], this.vertices[8], this.vertices[0]]),
-			// bottom
-			new Face().setVertices([this.vertices[11], this.vertices[1], this.vertices[3]]),
-			new Face().setVertices([this.vertices[11], this.vertices[3], this.vertices[5]]),
-			new Face().setVertices([this.vertices[11], this.vertices[5], this.vertices[7]]),
-			new Face().setVertices([this.vertices[11], this.vertices[7], this.vertices[9]]),
-			new Face().setVertices([this.vertices[11], this.vertices[9], this.vertices[1]])
-		];
+			[0, 1, 5],
+			[0, 4, 1],
+			[0, 5, 8],
+			[0, 10, 4],
+			[0, 8, 10],
+			[1, 6, 7],
+			[1, 7, 8],
+			[1, 8, 5],
+			[1, 10, 6],
+			[1, 11, 10],
+			[2, 3, 4],
+			[2, 4, 9],
+			[2, 5, 3],
+			[2, 9, 11],
+			[2, 11, 5],
+			[3, 6, 7],
+			[3, 7, 9],
+			[3, 9, 4],
+			[3, 10, 6],
+			[3, 11, 10]
+		].map((face) =>
+			new Face().setVertices([
+				this.vertices[face[0]],
+				this.vertices[face[1]],
+				this.vertices[face[2]]
+			])
+		);
 	}
 }
